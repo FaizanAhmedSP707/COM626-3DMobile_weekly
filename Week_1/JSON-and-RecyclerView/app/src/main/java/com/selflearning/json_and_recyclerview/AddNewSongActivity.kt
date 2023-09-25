2b07be2f6f122e7a8d623befc575b0cc1cbc30bf
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-09-25 16:42:17 +0100

    Initial commit of application base that will be modified to use RecyclerView when displaying the results.

---
+++ Week_1/JSON-and-RecyclerView/app/src/main/java/com/selflearning/json_and_recyclerview/AddNewSongActivity.kt
@@ -0,0 +1,57 @@
+package com.selflearning.json_and_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
+import android.widget.Toast
+
+class AddNewSongActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addsong)
+
+        val songTitleField = findViewById<EditText>(R.id.songTitle_field)
+        val songArtistField = findViewById<EditText>(R.id.songArtist_field)
+        val songYearField = findViewById<EditText>(R.id.songYear_field)
+        val addNewSongBtn = findViewById<Button>(R.id.addSongBtn)
+
+        addNewSongBtn.setOnClickListener {
+            // Check for non-null values first and display error messages as appropriate
+            if(songTitleField?.text.toString().trim() == "") {
+                songTitleField.error = "The title field cannot be empty!"
+            } else if(songArtistField?.text.toString().trim() == "") {
+                songArtistField.error = "The artist field cannot be empty!"
+            } else if(songYearField?.text.toString().trim() == "") {
+                songYearField.error = "The year field cannot be empty!"
+            } else {
+
+                // Obtain the values entered in each of the EditTexts to use them for sending a POST request to the server
+                val songtitle = songTitleField.text.toString()
+                val songartist = songArtistField.text.toString()
+                val songyear = songYearField.text.toString().toInt()
+
+                // Do the web stuff here: setting the URL and sending the POST request
+                val url = "http://192.168.98.222:3000/song/create"
+                val httppostData = listOf("title" to songtitle, "artist" to songartist, "year" to songyear)
+                url.httpPost(httppostData).response {request, response, result ->
+                    when(result) {
+                        is Result.Success -> {
+                            // If the POST request is successful
+                            Toast.makeText(this, result.get().decodeToString(), Toast.LENGTH_LONG).show()
+                        }
+
+                        is Result.Failure -> {
+                            // If the POST request failed
+                            Toast.makeText(this, result.error.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Week_1/JSON-and-RecyclerView/app/src/main/java/com/selflearning/json_and_recyclerview/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.selflearning.json_and_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.github.kittinunf.fuel.core.Parameters
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.fuel.gson.responseObject
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
+
+import android.content.Intent
+import android.view.MenuItem
+import android.view.Menu
+import android.view.MenuInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nameEntry = findViewById<EditText>(R.id.songArtistSearchName)
+        val songSearchBtn = findViewById<Button>(R.id.searchSongArtistBtn)
+        val resultView = findViewById<TextView>(R.id.webTextDisplay)
+        resultView.movementMethod = ScrollingMovementMethod()
+        // Button listener that will send requests to the server
+        songSearchBtn.setOnClickListener {
+
+            // First attempt to access the value entered in the EditText
+            if(nameEntry?.text.toString().trim() == ""){
+                // Right now, we want the user to enter some text into this filed before submitting
+                Toast.makeText(this, "No name entered in the text field above.", Toast.LENGTH_SHORT).show()
+                nameEntry.error = "Artist name field shouldn't be empty."
+            } else {
+                // The EditText isn't empty, so get the value and then send the request to the server
+                val artistName = nameEntry.text.toString()
+
+                // Use 10.0.2.2:3000 when running the server on the same machine as the app
+                val url = "http://192.168.243.222:3000/artist/${artistName}"
+                url.httpGet().responseObject<List<Song>> { request, response, result ->
+
+                    when(result) {
+                        is Result.Success -> {
+                            // Parsing not done right now, we'll just be displaying raw data
+                            //resultView.text = result.get().decodeToString()
+
+                            // Parsing done using GSON
+                            val displaytext = result.get().map {
+                                "${it.title} by ${it.artist}, " +
+                                        "ID: ${it.ID}, \n" +
+                                        "Day: ${it.day}, " +
+                                        "Month: ${it.month}, " +
+                                        "Year: ${it.year}, \n" +
+                                        "Chart: ${it.chart}, " +
+                                        "Likes: ${it.likes}, " +
+                                        "Downloads: ${it.downloads}, " +
+                                        "Quantity: ${it.quantity}, \n" +
+                                        "Review: ${it.review}" + "\n"
+                            }.joinToString("\n")
+                            // Set the text to the TextView
+                            resultView.text = displaytext
+                        }
+
+                        is Result.Failure -> {
+                            // Failure is when there is an HTTP error
+                            resultView.text = "ERROR ${result.error.message}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.addsongActivity -> {
+                val intent = Intent(this, AddNewSongActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return false
+    }
+}
+
+data class Song (
+    val title: String,
+    val artist: String,
+    val day: Int,
+    val month: String,
+    val year: Int,
+    val chart: Int,
+    val likes: Int,
+    val downloads: Int,
+    val ID: Int,
+    val review: String,
+    val quantity: Int
+)
