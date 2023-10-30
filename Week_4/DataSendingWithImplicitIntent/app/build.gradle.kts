4a701f574d7c70f10cda68f85ee33dadca7d4e69
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-10-30 13:20:50 +0000

    Amended compileSdk option from 33 to 34 to aid compilation of the app using the latest core libraries of Android.

--- Week_4/DataSendingWithImplicitIntent/app/build.gradle.kts
+++ Week_4/DataSendingWithImplicitIntent/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.unilearning.forwardimplicitapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.unilearning.forwardimplicitapp"
