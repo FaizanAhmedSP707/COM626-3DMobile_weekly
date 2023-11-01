9c6746c1d3dc96c4d1f99be29a5cb3e8cc8a415c
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-01 10:05:38 +0000

    Made a change to the global accel variable from a lateinit to a nullable Sensor, and removed the comment on line 24.

---
+++ Week_2/LifecycleObserverStatus/app/src/main/java/com/unilearning/lifecycleobserverapp/LifecycleStatusObserver.kt
@@ -0,0 +1,25 @@
+package com.unilearning.lifecycleobserverapp
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class LifecycleStatusObserver(val lifeviewModel: LifecycleViewModel): DefaultLifecycleObserver{
+
+    override fun onCreate(owner: LifecycleOwner) {
+
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        
+    }
+}
