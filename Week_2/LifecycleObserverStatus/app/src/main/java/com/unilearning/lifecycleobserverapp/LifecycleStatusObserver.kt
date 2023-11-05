55eb50e8ddc3e01c4e44cdb57dd2c0dfb6c0cad6
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-05 13:22:14 +0000

    Wrote code to add a different status message to the view model every time the application undergoes a change of state. This message will be seen in the main activity.

--- Week_2/LifecycleObserverStatus/app/src/main/java/com/unilearning/lifecycleobserverapp/LifecycleStatusObserver.kt
+++ Week_2/LifecycleObserverStatus/app/src/main/java/com/unilearning/lifecycleobserverapp/LifecycleStatusObserver.kt
@@ -6,20 +6,33 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class LifecycleStatusObserver(val lifeViewModel: LifecycleViewModel): DefaultLifecycleObserver {
-
+    private var message = false
     override fun onCreate(owner: LifecycleOwner) {
-
+        // When the activity is first launched, this will run first
+        message = true
+        lifeViewModel.addStatusMessage("Started")
     }
 
     override fun onPause(owner: LifecycleOwner) {
-
+        // When the activity is paused
+        message = false
+        lifeViewModel.addStatusMessage("Paused")
     }
 
     override fun onResume(owner: LifecycleOwner) {
-
+        // When the activity is resumed again after another activity or app was closed when this app was running
+        message = false
+        lifeViewModel.addStatusMessage("Resumed")
     }
 
     override fun onDestroy(owner: LifecycleOwner) {
-
+        /*
+        When the application is destroyed. This doesn't mean that you have to close the app completely
+        to see this message, and obviously this message won't show up in that case. If the orientation of
+        the device is changed, the current application will be destroyed and created anew. That is when
+        this message will be seen
+        */
+        message = false
+        lifeViewModel.addStatusMessage("Destroyed")
     }
 }
