465689214cec850640a59d102322f9d2049dc929
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-05 16:52:22 +0000

    Completed the code for the main acivity to display the changes in application lifecycle, using a viewModel and an observer interface attached to the main activity to observe the changes in the app's lifecycle.

--- Week_2/LifecycleObserverStatus/app/src/main/java/com/unilearning/lifecycleobserverapp/MainActivity.kt
+++ Week_2/LifecycleObserverStatus/app/src/main/java/com/unilearning/lifecycleobserverapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.unilearning.lifecycleobserverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+
+    val lifeViewModel: LifecycleViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val observer = LifecycleStatusObserver(lifeViewModel)
+        this.lifecycle.addObserver(observer)
+
+        lifeViewModel.statesLive.observe(this, Observer{
+            findViewById<TextView>(R.id.tvStateLister).movementMethod = ScrollingMovementMethod()
+            findViewById<TextView>(R.id.tvStateLister).text = it.joinToString("\n")
+        })
     }
 }
