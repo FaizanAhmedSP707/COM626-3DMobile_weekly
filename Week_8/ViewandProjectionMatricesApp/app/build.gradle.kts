175d0af035b70c2512255c276057125e471a71c5
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-13 11:51:52 +0000

    Updated the compileSdk option to 34 to prevent errors during app compilation.

--- Week_8/ViewandProjectionMatricesApp/app/build.gradle.kts
+++ Week_8/ViewandProjectionMatricesApp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.unilearning.depthviewapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.unilearning.depthviewapp"
