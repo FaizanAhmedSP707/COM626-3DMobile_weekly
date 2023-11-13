101144b4733f8b99c484feb0c94faa9b39866966
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-13 10:50:27 +0000

    Improved the compileSdk option to 34 in order to compile the app.

--- Week_7/OpenGL_StarterApp/app/build.gradle.kts
+++ Week_7/OpenGL_StarterApp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.unilearning.opengl_app1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.unilearning.opengl_app1"
