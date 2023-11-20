ef13c58b99f74bbe2d975697d63112609df968aa
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-20 13:46:01 +0000

    Added code for handling events on all of the buttons (except two) defined in the layout of the main activity.

--- Week_9/DepthViewandRotation/app/src/main/java/com/unilearning/opengl_rotationapp/MainActivity.kt
+++ Week_9/DepthViewandRotation/app/src/main/java/com/unilearning/opengl_rotationapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.unilearning.opengl_rotationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Creating a reference to the GLView class so that the camera can be manipulated
+        val glView = findViewById<OpenGLView>(R.id.glview)
+
+        findViewById<Button>(R.id.minusX).setOnClickListener {
+            glView.camera.translate(-1f, 0f, 0f)
+        }
+        findViewById<Button>(R.id.plusX).setOnClickListener {
+            glView.camera.translate(1f, 0f, 0f)
+        }
+        findViewById<Button>(R.id.minusY).setOnClickListener {
+            glView.camera.translate(0f, -1f, 0f)
+        }
+        findViewById<Button>(R.id.plusY).setOnClickListener {
+            glView.camera.translate(0f, 1f, 0f)
+        }
+        findViewById<Button>(R.id.minusZ).setOnClickListener {
+            glView.camera.translate(0f, 0f, -1f)
+        }
+        findViewById<Button>(R.id.plusZ).setOnClickListener {
+            glView.camera.translate(0f, 0f, 1f)
+        }
+        findViewById<Button>(R.id.cwRotation).setOnClickListener {
+            glView.camera.rotate(-10f)
+        }
+        findViewById<Button>(R.id.acwRotation).setOnClickListener {
+            glView.camera.rotate(10f)
+        }
     }
 }
