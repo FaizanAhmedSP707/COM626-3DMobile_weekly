a122f310d19d30956e2d2d7d976f54db91fffccd
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-27 13:30:15 +0000

    Updated to point to a newer version of the .aar file for the project.

--- Week_10/app/build.gradle.kts
+++ Week_10/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("libs/app-debugv12.aar"))
+    implementation(files("libs/app-debugv13.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
