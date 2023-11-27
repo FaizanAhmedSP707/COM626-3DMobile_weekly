9cb206ce49abd04eb0874f0139ca96ee2e259901
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-11-27 09:51:28 +0000

    Partial code commit, app doesn't work at the moment.

--- Week_6/CameraX_StarterApp/app/src/main/java/com/unilearning/camerax_app/MainActivity.kt
+++ Week_6/CameraX_StarterApp/app/src/main/java/com/unilearning/camerax_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.unilearning.camerax_app
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            // Do something for picture storage
+        } else {
+            //
+        }
     }
 }
