68c1bb812a0938f3819b8d95c2590c58a0a4a730
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-12-03 11:40:12 +0000

    Wrote partial code for the camera app, needs more stuff.

--- Week_6/CameraX_StarterApp/app/src/main/java/com/unilearning/camerax_app/MainActivity.kt
+++ Week_6/CameraX_StarterApp/app/src/main/java/com/unilearning/camerax_app/MainActivity.kt
@@ -3,8 +3,17 @@ package com.unilearning.camerax_app
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    // An image capture object that we will need for when the image is captured
+    val imageCapture : ImageCapture? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +23,28 @@ class MainActivity : AppCompatActivity() {
         } else {
             //
         }
+    }
+
+    private fun startCamera() {
+        val lifecycleOwner = this
+        val previewCam = findViewById<PreviewView>(R.id.previewCamView)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build().also {
+                    // previewCam is a UI object representing the preview, so we use
+                    // findViewByID() to use it
+                    it.setSurfaceProvider(previewCam.surfaceProvider)
+                }
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
+                } catch (e: Exception) {
+                    Log.d("CAMERAX1", e.stackTraceToString())
+                }
+            }, ContextCompat.getMainExecutor(this)
+        )
     }
 }
