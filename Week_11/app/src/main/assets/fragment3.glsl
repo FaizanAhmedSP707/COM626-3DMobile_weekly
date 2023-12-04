1e7628b7857eaee8282cf525782dcec55a237130
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-12-04 10:53:58 +0000

    Fragment shader for textures.

---
+++ Week_11/app/src/main/assets/fragment3.glsl
@@ -0,0 +1,10 @@
+// For dealing with textures
+#extension GL_OES_EGL_image_external: require
+precision mediump float;
+
+varying vec2 vTextureValue;
+uniform samplerExternalOES uTexture;
+
+void main(void){
+    gl_FragColor = texture2D(uTexture, vTextureValue);
+}
