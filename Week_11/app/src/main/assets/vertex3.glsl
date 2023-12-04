aad78136105fe2eefe9deb916e7e75e4fd541818
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-12-04 13:38:46 +0000

    Negated the t-coordinate to display the camera feed properly.

--- Week_11/app/src/main/assets/vertex3.glsl
+++ Week_11/app/src/main/assets/vertex3.glsl
@@ -7,5 +7,10 @@ varying vec2 vTextureValue;
 
 void main(void){
     gl_Position = aVertex;
-    vTextureValue = vec2(0.5*(1.0 + aVertex.x), 0.5*(1.0 + aVertex.y));
+    vTextureValue = vec2(0.5*(1.0 + aVertex.x), 0.5*(1.0 + (-aVertex.y)));
 }
+/*
+Need to negate the T-coordinate due to the way the video memory is presented. The video
+memory is presented from top to bottom, whereas S&T coordinates start from bottom to top.
+Thus, we need to negate the T-coordinate so that the camera feed displays properly.
+*/
